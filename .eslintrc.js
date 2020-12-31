1be60572ca7ce83863bd41c2838c75dca136132f
Vinicios Wentz <vinicios@wentz.io>
2020-12-31 17:27:46 -0300

    feat: ensure SignupController return 400 and erro if no email is provided

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-redeclare': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off'
   }
 }
